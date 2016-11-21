43aa12d877df640f5a49902b15b63c04f540dec0
josephhschwartz <joseph.h.schwartz@gmail.com>
2016-11-21 08:57:55 -0500

    Create startuplinux.sh

---
+++ startuplinux.sh
@@ -0,0 +1,6 @@
+sudo apt-get install git
+git clone https://github.com/josephhschwartz/google-cloud-trial.git
+sudo apt-get install -y python-nltk
+sudo apt-get install -y python-scipy
+sudo apt-get install -y python-pip
+sudo pip install scikit-learn==0.15.2
