69d2c3ba34a811ccab051c9a2db3481843610853
josephhschwartz <joseph.h.schwartz@gmail.com>
2016-11-21 20:49:09 -0500

    Update startuplinux.sh

--- startuplinux.sh
+++ startuplinux.sh
@@ -4,3 +4,5 @@ sudo apt-get install -y python-nltk
 sudo apt-get install -y python-scipy
 sudo apt-get install -y python-pip
 sudo pip install scikit-learn==0.15.2
+wget "https://storage.googleapis.com/this-is-my-test-bucket/_cf43ec20fb60fa894c2a0ec84a791f84_Assignment1.tar.gz"
+tar -zxvf _cf43ec20fb60fa894c2a0ec84a791f84_Assignment1.tar.gz
